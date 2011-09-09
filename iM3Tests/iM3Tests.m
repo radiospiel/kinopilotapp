a1c4aabf81015bacfcf0fc33f2c7f0f5c24d99e3
eno <eno@open-lab.org>
2011-09-09 22:33:07 +0200

    Adds iM projects

---
+++ iM3Tests/iM3Tests.m
@@ -0,0 +1,32 @@
+//
+//  iM3Tests.m
+//  iM3Tests
+//
+//  Created by Enrico Thierbach on 09.09.11.
+//  Copyright (c) 2011 n/a. All rights reserved.
+//
+
+#import "iM3Tests.h"
+
+@implementation iM3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iM3Tests");
+}
+
+@end
