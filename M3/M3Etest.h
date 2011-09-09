084213dac49766f7fa7ab4b79aa8d7906c8913db
eno <eno@open-lab.org>
2011-09-09 16:36:36 +0200

    Fixes tests for automatically converted objects

--- M3/M3Etest.h
+++ M3/M3Etest.h
@@ -30,15 +30,18 @@ void m3_etest_failed(NSString* msg, const char* file, int line);
 #define m3_assert(expr, msg)        do { if(!expr) m3_etest_failed(msg, __FILE__, __LINE__); } while(0)
 #define m3_msg(fmt, p1, p2)         [ NSString stringWithFormat:fmt, p1, p2]
 
-#ifdef UNDERSCORE_HH
-
-#define assert_equal(a, e)          m3_assert([_.object(a) isEqual: _.object(e)],   m3_msg(@"%@ should be equal to %@", (a), (e)))
-#define assert_not_equal(a, e)      m3_assert(![_.object(a) isEqual: _.object(e)],  m3_msg(@"%@ should not be equal to %@", (a), (e)))
-
-#endif
-
 #define assert_equal_pod(a, e)      m3_assert((a == e),           m3_msg(@"%s should be equal to %s", #a, #e))
 #define assert_equal_objects(a, e)  m3_assert([(a) isEqual: (e)], m3_msg(@"%@ should be equal to %@", (a), (e)))
+
+#define assert_not_equal_pod(a, e)      m3_assert((a != e),           m3_msg(@"%s should not be equal to %s", #a, #e))
+#define assert_not_equal_objects(a, e)  m3_assert(![(a) isEqual: (e)], m3_msg(@"%@ should not be equal to %@", (a), (e)))
+
+#ifdef UNDERSCORE_HH
+
+#define assert_equal(a, e)          assert_equal_objects(_.object(a), _.object(e))
+#define assert_not_equal(a, e)      assert_not_equal_objects(_.object(a), _.object(e))
+
+#endif
 
 #define assert_true(a)              m3_assert((a), @"Should be true")
 #define assert_false(a)             m3_assert((a), @"Should be false")
