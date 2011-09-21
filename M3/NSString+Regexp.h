8a9cf8b9fa9b10bf1ffe867977eb84b9de33a408
eno <eno@open-lab.org>
2011-09-21 11:37:51 +0200

    String#matches returns an array of matches.

--- M3/NSString+Regexp.h
+++ M3/NSString+Regexp.h
@@ -2,7 +2,7 @@
 @interface NSString (Regex)
 
 - (NSString*) gsub: (id) regexp_or_string with: (NSString*) tmpl;
-- (NSUInteger) matches:(id)regexp_or_string;
+- (NSArray*) matches:(id)regexp_or_string;
 
 @end
 
--- M3/NSString+Regexp.mm
+++ M3/NSString+Regexp.mm
@@ -12,14 +12,23 @@
                                      withTemplate:replacement];
 }
 
-- (NSUInteger) matches:(id)regexp_or_string
+- (NSArray*) matches:(id)regexp_or_string
 {
-  NSRegularExpression* regexp = [ M3 regexp: regexp_or_string];
+  NSRegularExpression* regexp = [ M3 regexp:regexp_or_string];
+
+  NSArray* regexp_matches = [ regexp matchesInString:self
+                                             options:0 
+                                               range:NSMakeRange(0, self.length) ];
   
-  return [regexp numberOfMatchesInString: self
-                                  options: 0
-                                    range: NSMakeRange(0, self.length)];
-                                                     
+  if(regexp_matches.count == 0) return nil;
+  
+  NSMutableArray* matches = [NSMutableArray array];
+  for(NSTextCheckingResult* regexp_match in regexp_matches) {
+    NSString* match = [self substringWithRange:[regexp_match range]];
+    [matches addObject: match];
+  }
+  
+  return matches;
 }
 
         
@@ -55,10 +64,19 @@ ETest(Regexp)
 
 -(void)test_regexp
 {
-  assert_equal(1, [@"abc" matches: @"a"]);
-  assert_equal(1, [@"abc" matches: [M3 regexp: @"a"]]);
-  assert_equal(0, [@"abc" matches: [M3 regexp: @"A"]]);
-  assert_equal(1, [@"abc" matches: [M3 regexp: @"A" withOptions: NSRegularExpressionCaseInsensitive]]);
+  assert_true([@"abc" matches: @"a"]);
+  assert_true([@"abc" matches: [M3 regexp: @"a"]]);
+  assert_false([@"abc" matches: [M3 regexp: @"A"]]);
+  assert_true([@"abc" matches: [M3 regexp: @"A" withOptions: NSRegularExpressionCaseInsensitive]]);
+}
+
+-(void)test_regexp2
+{
+  assert_equal([@"abc abc" matches: @"abc"],    _.array("abc", "abc"));
+  assert_equal([@"ab abc" matches: @"abc"],     _.array("abc"));
+  assert_nil([@"ab abc" matches: @"xyz"]);
+  assert_equal([@"ab abc" matches: @"ab(c?)"],  _.array("ab", "abc"));
+  assert_equal([@"abc abc" matches: @"abc?"],   _.array("abc", "abc"));
 }
 
 @end
