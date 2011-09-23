1d7f3f4c02951c4fe673736208f10466f3c459d6
eno <eno@open-lab.org>
2011-09-23 12:20:18 +0200

    Adds sencha.io support for UIImageView+HTTP

--- M3/UIImageView+Http.h
+++ M3/UIImageView+Http.h
@@ -1,5 +1,9 @@
+#if TARGET_OS_IPHONE 
+
 @interface UIImageView(Http)
 
 @property (nonatomic,retain) NSString* imageURL;
 
 @end
+
+#endif
--- M3/UIImageView+Http.mm
+++ M3/UIImageView+Http.mm
@@ -2,6 +2,24 @@
 
 #import "M3.h"
 
+//
+// Enable sencha.io support? sencha.io delivers images in just the right size. This reduces 
+// memory usage on the device, but increases the time needed to fetch all the images.
+//
+// For more on sencha.io see http://www.sencha.com/learn/how-to-use-src-sencha-io/
+//
+
+#define USE_SENCHA_IO 1
+
+@implementation UIImage(M3Extensions)
+
+-(NSString*) inspects
+{
+  return [NSString stringWithFormat: @"<NSImage %dx%d>", (int)self.size.width, (int)self.size.height];
+}
+
+@end
+
 @implementation UIImageView(Http)
 
 -(NSString*)imageURL;
@@ -11,37 +29,43 @@
 
 -(void)setImage: (UIImage*)image fromURL: (NSString*)url 
 {
-  @synchronized(self) {
-    self.image = image;
-    [self instance_variable_set: @selector(imageURL) withValue:url];
-  }
+  self.image = image;
+  [self instance_variable_set: @selector(imageURL) withValue:url];
 }
 
 -(void)setImageWithAnimation: (UIImage*)image fromURL: (NSString*)url 
 {
-  @synchronized(self) {
-    // Add the image on top of the current image
-    UIImageView *overlay = [[UIImageView alloc] initWithImage:image];
-    
-    [self.superview addSubview: overlay];
-    overlay.frame = self.frame;
-    overlay.alpha = 0.0; // initially hide the image
-    
-    [UIView animateWithDuration:0.2
-                     animations:^{ overlay.alpha = 1.0; }
-                     completion:^(BOOL finished){
-                       self.image = overlay.image;
-                       [overlay removeFromSuperview];
-                     }];
-
-    [self instance_variable_set: @selector(imageURL) withValue:url];
-  }
+  // Add the image on top of the current image
+  UIImageView *overlay = [[UIImageView alloc] initWithImage:image];
+  
+  [self.superview addSubview: overlay];
+  overlay.frame = self.frame;
+  overlay.alpha = 0.0; // initially hide the image
+  
+  [UIView animateWithDuration:0.2
+                   animations:^{ overlay.alpha = 1.0; }
+                   completion:^(BOOL finished){
+                     self.image = overlay.image;
+                     [overlay removeFromSuperview];
+                   }];
+  
+  [self instance_variable_set: @selector(imageURL) withValue:url];
 }
 
 -(void)setImageURL: (NSString*)url;
 {
-  if(!url || [url isEqualToString: self.imageURL]) return;
+  if(!url) return;
 
+#if USE_SENCHA_IO
+  int width = self.frame.size.width;
+  int height = self.frame.size.height;
+  if(width > 0 && height > 0) {
+    // TODO: add proper support for shards
+    NSString* shard = @"";
+    url = [NSString stringWithFormat: @"http://src%@.sencha.io/%d/%d/%@", shard, width, height, url];
+  }
+#endif
+  
   NSString* cachePath = _.join("$cache/", [M3 md5: url], ".bin");
 
   if([M3 fileExists: cachePath]) {          // read image from file, if it exists
@@ -68,5 +92,4 @@
 }
 
 @end
-
 #endif
