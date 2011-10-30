886ffad07169aca2d0bfeed87a4fd21a762d74dc
eno <eno@open-lab.org>
2011-10-30 15:46:10 +0100

    Adds invitation email

--- iM3Example/SchedulesShowController.mm
+++ iM3Example/SchedulesShowController.mm
@@ -1,6 +1,9 @@
 #import "AppDelegate.h"
 #import "SchedulesShowController.h"
 
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMailComposeViewController.h>
+
 @implementation SchedulesShowController
 
 -(void)setUrl: (NSString*)urlString
@@ -23,6 +26,49 @@
   [self addAction:@"Facebook" withURL: _.join(@"/share/facebook?schedule_id=", schedule_id)];
   [self addAction:@"Email"    withURL: _.join(@"/share/email?schedule_id=", schedule_id)];
   [self addAction:@"Kalender" withURL: _.join(@"/share/calendar?schedule_id=", schedule_id)];
+}
+
+-(void)sendEmail
+{
+  NSString* schedule_id = [self.url.to_url param: @"schedule_id"];
+  NSDictionary* schedule = [app.chairDB.schedules get: schedule_id];
+
+  NSNumber* time = [schedule objectForKey: @"time"];
+  NSDictionary* movie = [app.chairDB.movies get: [schedule objectForKey:@"movie_id"]];
+  NSDictionary* theater = [app.chairDB.theaters get: [schedule objectForKey:@"theater_id"]];
+  
+  NSArray* latlong = [theater objectForKey:@"latlong"];
+
+  NSDictionary* context = _.hash(
+                              @"movie", movie,
+                              @"theater", theater,
+                              @"nice_time",   [time.to_date stringWithFormat: @"dd. MMM HH:mm"],
+                              @"coordinates",   [latlong componentsJoinedByString: @","]
+                              );  
+  
+  // -- compose HTML message.
+
+  MFMailComposeViewController* mailCo = [[[MFMailComposeViewController alloc] init] autorelease];
+  
+  [mailCo setSubject: [@"{{nice_time}}: {{movie.title}} im {{theater.name}}" interpolate: context]];
+
+  NSString* tmpl = [M3 read: @"$app/invitation_mail.html"];
+  
+  [mailCo setMessageBody: [tmpl interpolate: context] isHTML:YES]; 
+  
+  // -- show message composer.
+  
+  [app.window.rootViewController presentModalViewController:mailCo animated:YES];
+}
+
+-(void)openAction:(NSString*)label
+{
+  if([label isEqualToString:@"Email"]) {
+    [self sendEmail];
+    return;
+  }
+  
+  [super openAction:label];
 }
 
 @end
