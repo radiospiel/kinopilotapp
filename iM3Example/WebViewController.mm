cf5e2a62736e97ed9ec6f537e821745bd492e013
eno <eno@open-lab.org>
2012-01-13 00:04:54 +0100

    Some cleanup

--- iM3Example/WebViewController.mm
+++ iM3Example/WebViewController.mm
@@ -13,20 +13,6 @@
 
 @implementation WebViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-  return self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-  // Releases the view if it doesn't have a superview.
-  [super didReceiveMemoryWarning];
-  
-  // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
