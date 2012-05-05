6c578961ac8031347ee0ccbba0a95753897f0425
eno <eno@open-lab.org>
2012-05-05 19:14:46 +0200

    Revert "Removed UserVoice code."
    This reverts commit d4666f06d001996b38197ddb0f3bca6addde12a9.

---
+++ iM3Example/UserVoiceController.mm
@@ -0,0 +1,31 @@
+#import "M3AppDelegate.h"
+#import "UserVoice.h"
+
+@interface UserVoiceController: NSObject
+@property (retain,nonatomic) NSString* url;
+@end
+
+@implementation UserVoiceController
+
+@synthesize url = _url;
+
+-(void)perform
+{
+  [app oneTimeHint: @"Danke für Dein Feedback!\n\n"
+                     "Wir benutzen das Feedbacksystem von UserVoice - "
+                     "um nitzumachen, musst Du Dich dort eventuell registrieren." 
+           withKey: @"user_voice_register" 
+     beforeCalling: ^{
+       [UserVoice presentUserVoiceModalViewControllerForParent: app.topMostController
+                                                       andSite:@"http://kinopilot.uservoice.com/"
+                                                        andKey:@"K3EoDPEyFEipbUX1daA"
+                                                     andSecret:@"gHkvOHmPkqEA5z6OXi6h0aLQdQLvAd6h2PqMq1cUkVM"];
+     }
+  ];
+}
+
+-(NSString*)title
+{
+  return @"Feedback";
+}
+@end
