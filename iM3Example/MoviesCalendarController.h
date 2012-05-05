b90f53eb05526e21ed408d5c0fdefe4c25cba68f
eno <eno@open-lab.org>
2012-05-05 13:54:56 +0200

    Adds /movies/calendar controller

---
+++ iM3Example/MoviesCalendarController.h
@@ -0,0 +1,12 @@
+//
+//  MoviesListController.h
+//  M3
+//
+//  Created by Enrico Thierbach on 22.09.11.
+//  Copyright (c) 2011 n/a. All rights reserved.
+//
+
+#import "M3ListViewController.h"
+
+@interface MoviesCalendarController: M3ListViewController
+@end
