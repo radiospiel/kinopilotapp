f3e61bbc999fa8293917683f61d50932b657b9ad
eno <eno@open-lab.org>
2012-05-15 15:37:24 +0200

    Reorganized code for better code sharing between multiple Applications

---
+++ flk-kinopilot/Freiluft-Kinopilot/Freiluft-Kinopilot/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  Freiluft-Kinopilot
+//
+//  Created by Enrico Thierbach on 15.05.12.
+//  Copyright (c) 2012 n/a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
