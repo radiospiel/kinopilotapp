8054bc4386b2bb0f11148ca5986565fbc9eb1fbf
eno <eno@open-lab.org>
2012-05-15 22:56:16 +0200

    Refactored InfoController

--- Shared/InfoController.mm
+++ Shared/InfoController.mm
@@ -8,19 +8,15 @@
 
 #import "AppBase.h"
 
-@interface InfoController : M3TableViewController
-
-@end
-
 /* 
  * This cell shows one value (i.e. one piece of text). The text might span
  * over multiple lines.
  */
 
-@interface InfoControllerCellOneValue: M3TableViewCell
+@interface InfoTextCell: M3TableViewCell
 @end
 
-@implementation InfoControllerCellOneValue
+@implementation InfoTextCell
 
 +(void)initialize
 {
@@ -42,21 +38,19 @@
 
 -(void)setKey: (NSString*)key
 {
-  [super setKey: key];
-  
   M3AssertKindOf(key, NSString);
-  self.textLabel.text = key;
+
+  [super setKey: key];
+  self.textLabel.text = self.key;
 }
 
 - (CGFloat)wantsHeight
 {
   [self layoutSubviews];
 
-  NSString* text = self.key;
-  
-  CGSize stringSize = [text sizeWithFont:self.textLabel.font
-                       constrainedToSize:CGSizeMake(260, 9999) 
-                           lineBreakMode:UILineBreakModeWordWrap ];
+  CGSize stringSize = [self.key sizeWithFont:self.textLabel.font
+                           constrainedToSize:CGSizeMake(260, 9999) 
+                               lineBreakMode:UILineBreakModeWordWrap ];
 
   return stringSize.height + 20;
 }
@@ -67,10 +61,10 @@
  * This cell shows a label and a value
  */
 
-@interface InfoControllerCellTwoValues: M3TableViewCell
+@interface InfoTextValueCell: M3TableViewCell
 @end
 
-@implementation InfoControllerCellTwoValues
+@implementation InfoTextValueCell
 
 +(void)initialize
 {
@@ -89,21 +83,32 @@
   return 35; 
 }
 
+-(NSString*)resolveValue: (NSString*)infoKey
+{
+  NSString* text = [app.infoDictionary objectForKey:infoKey];
+  if(!text) return infoKey;
+
+  return [text description];
+}
+
 -(void)setKey: (NSArray*)key
 {
   [super setKey: key];
+
+  NSString* label = key.first;
+  NSString* infoKey = key.second;
   
-  self.textLabel.text = key.first;
+  self.textLabel.text = label;
   self.textLabel.textAlignment = UITextAlignmentRight;
-  
-  NSString* text = [key.last description];
-  if([text matches:@"(http://|https://|mailto:)(.*)"]) {
-    [self.detailTextLabel onTapOpen: text];
-    text = $2;
+
+  NSString* value = [self resolveValue: infoKey];
+  if([value matches:@"(http://|https://|mailto:)(.*)"]) {
+    [self.detailTextLabel onTapOpen: value];
+    value = $2;
   }
 
   self.detailTextLabel.textAlignment = UITextAlignmentLeft;
-  self.detailTextLabel.text = text;
+  self.detailTextLabel.text = value;
   self.detailTextLabel.textColor = [UIColor colorWithName: @"#385487"];
 }
 
@@ -127,61 +132,51 @@
  * The data source for the InfoController
  */
 @interface InfoControllerDataSource: M3TableViewDataSource
-
 @end
 
 @implementation InfoControllerDataSource
 
--(id)initWithSection: (NSString*) section
+-(Class) cellClassForKey: (id)key;
+{
+  if(![key isKindOfClass:[NSArray class]])
+    return [InfoTextCell class];
+
+  return [InfoTextValueCell class];
+}
+
+-(Class)cellValueForKey: (id)key
+{
+  return key;
+}
+
+-(id)init
 {
   self = [super init];
-  if(!self) return nil;
-
-  NSDictionary* infoDictionary = app.infoDictionary;
-  NSArray* infoSections = [app.config objectForKey: section];
   
+  NSArray* infoSections = [app.config objectForKey: @"info"];
   M3AssertKindOf(infoSections, NSArray);
+
   for(NSDictionary* section in infoSections) {
     M3AssertKindOf(section, NSDictionary);
-    
-    id content = [section objectForKey:@"content"];
-    if(!content)
-      content = [NSArray array];
 
-    content = [content mapUsingBlock:^id(NSArray* entry) {
-      if(![entry isKindOfClass:[NSArray class]]) return entry;
-      id key = entry.last;
-      id value = [infoDictionary objectForKey:key];
-      
-      return [NSArray arrayWithObjects:entry.first, value ? value : key, nil];
-    }];
-    // Read "header", "footer", and "index" from the configuration.
-    [self addSection: content withOptions: section];
+    [self addSection: [section objectForKey:@"content"]   // fetch "content" from \a section
+         withOptions: section];                           // use "header", "footer", and "index" from \a section.
   }
   
   return self;
 }
 
--(Class) cellClassForKey: (NSArray*)key;
-{
-  if([key isKindOfClass:[NSArray class]])
-    return [InfoControllerCellTwoValues class];
+@end
 
-  return [InfoControllerCellOneValue class];
-}
-
+@interface InfoController: M3TableViewController
 @end
 
 @implementation InfoController
 
 -(id)init
 { 
-  self = [super initWithStyle: UITableViewStyleGrouped]; 
-
-  if(self) {
-    [app on: @selector(updated) notify:self with:@selector(reload)];
-  }
-
+  self = [super initWithStyle: UITableViewStyleGrouped];
+  [app on: @selector(updated) notify:self with:@selector(reload)];
   return self;
 }
 
@@ -189,11 +184,7 @@
 
 -(void)reloadURL
 {
-  NSURL* url = self.url.to_url;
-  
-  NSString* section = [url param:@"section"];
-  if(!section) section = @"about";
-  self.dataSource = [[[InfoControllerDataSource alloc]initWithSection:section]autorelease];
+  self.dataSource = [[[InfoControllerDataSource alloc]init]autorelease];
 }
 
 -(NSString*)title
