radiospiel/kinopilotapp

commit 72a4ae80121fe0e15d135b6ad974b337b68ff53b

Make sure cinemas get sorted properly

Shared/TheatersListController.mm
@@ -34,14 +34,14 @@
     NSString* sql;
     
     if([filter isEqualToString:@"fav"]) {
-      sql =  @"SELECT theaters._id, theaters.name FROM theaters "
+      sql =  @"SELECT theaters._id, theaters.name, theaters.sortkey FROM theaters "
               "INNER JOIN flags ON flags.key_id=theaters._id "
               "LEFT JOIN schedules ON schedules.theater_id=theaters._id "
               "LEFT JOIN movies ON schedules.movie_id=movies._id "
               "GROUP BY theaters._id ";
     }
     else {
-      sql =  @"SELECT theaters._id, theaters.name FROM theaters "
+      sql =  @"SELECT theaters._id, theaters.name, theaters.sortkey FROM theaters "
               "LEFT JOIN schedules ON schedules.theater_id=theaters._id "
               "LEFT JOIN movies ON schedules.movie_id=movies._id "
               "GROUP BY theaters._id ";

commit c5aacf3f5323f7fefa8e873dc24ce1b5535b2518

theater list works if there is no sortkey in the dataset.
#72a4ae80 broke theater list with old dataset, this patch fixes it. It should now work regardless of old and new data sets.

Shared/TheatersListController.mm
@@ -34,14 +34,14 @@
     NSString* sql;
     
     if([filter isEqualToString:@"fav"]) {
-      sql =  @"SELECT theaters._id, theaters.name, theaters.sortkey FROM theaters "
+      sql =  @"SELECT theaters.* FROM theaters "
               "INNER JOIN flags ON flags.key_id=theaters._id "
               "LEFT JOIN schedules ON schedules.theater_id=theaters._id "
               "LEFT JOIN movies ON schedules.movie_id=movies._id "
               "GROUP BY theaters._id ";
     }
     else {
-      sql =  @"SELECT theaters._id, theaters.name, theaters.sortkey FROM theaters "
+      sql =  @"SELECT theaters.* FROM theaters "
               "LEFT JOIN schedules ON schedules.theater_id=theaters._id "
               "LEFT JOIN movies ON schedules.movie_id=movies._id "
               "GROUP BY theaters._id ";